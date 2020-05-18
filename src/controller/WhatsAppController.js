115c6eaa4c214283288ba914cd69c9c4edb1b9fc
evandrop <evandro@optiplex.com.br>
2020-05-18 15:12:59 -0300

    Primeira Versão Whatsapp Clone

---
+++ src/controller/WhatsAppController.js
@@ -0,0 +1,417 @@
+class WhatsAppController {
+
+    constructor() {
+
+        console.log('WhatsAppController ok');
+
+        this.elementsPrototype();
+        this.loadElements(); // Gera todos os elementos a partir do doc HTML
+        this.initEvents();
+    };
+
+    elementsPrototype() {
+    
+        Element.prototype.hide = function() { 
+            this.style.display = 'none';
+            return this;
+        };
+
+        Element.prototype.show = function() {
+            this.style.display = 'block';
+            return this;
+        };
+
+        Element.prototype.toggle = function() {
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        };
+
+        Element.prototype.on = function(events, fn) {
+            events.split(' ').forEach(event => {
+
+                this.addEventListener(event, fn); // adiciona um ou multiplos eventos ao elemento
+
+            });
+            return this;
+
+        };
+
+        Element.prototype.css = function(styles) {
+
+              for (let name in styles) {
+                    this.style[name] = styles[name]; // altera CSS de um elemento
+              }  
+              return this;
+
+        };
+
+        Element.prototype.addClass = function(name) {
+
+            this.classList.add(name);
+            return this;
+
+        };
+
+        Element.prototype.removeClass = function(name) {
+
+            this.classList.remove(name);
+            return this;
+        };
+
+        Element.prototype.toggleClass = function(name) {
+
+            this.classList.toggle(name);
+            return this;
+        };
+
+        Element.prototype.hasClass = function(name) {
+
+            return this.classList.contains(name);
+
+        };
+
+        HTMLFormElement.prototype.getForm = function () {
+
+            return new FormData(this);
+
+        }
+
+        HTMLFormElement.prototype.toJSON = function () {
+
+            let json = {};
+            
+            this.getForm().forEach((value, key) => {
+
+                json[key] = value;
+
+            });
+
+            return json;
+        }
+    
+
+
+
+    };
+
+    loadElements(){
+
+        this.el = {};  
+        
+        document.querySelectorAll('[id]').forEach(element => {
+
+            this.el[Format.getCamelCase(element.id)] = element; // Cria um elemento a partir do id do HTML
+
+        });
+
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() =>  {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+
+        });   
+
+        this.el.btnNewContact.on('click', e => {
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() =>  {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+
+            this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e=> {
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if (e.key === 'Enter') {
+
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+
+            }
+        })
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTL);
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact);
+
+        });
+
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+
+            item.on('click', e => {
+
+                this.el.home.hide();
+                this.el.main.css({
+                    display: 'flex' 
+                });
+
+            });
+        });
+
+        this.el.btnAttach.on('click', e => {
+
+            e.stopPropagation();  // para a execução dos eventos em cadeia
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+
+        });
+
+        this.el.btnAttachPhoto.on('click', e => {
+
+            this.el.inputPhoto.click();
+
+        });
+
+        this.el.inputPhoto.on('change', e => {
+
+            console.log(this.el.inputPhoto.files);
+
+            [...this.el.inputPhoto.files].forEach(file => {
+
+                console.log(file);
+
+            });
+
+        });
+
+        this.el.btnAttachCamera.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height':'calc(100% - 120px)'
+            });
+
+            this._camera = new CameraController(this.el.videoCamera);  
+ 
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+
+            this.closeAllMainPanel(true);
+
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+
+            console.log('Take Picture');
+
+        });
+
+
+        this.el.btnAttachDocument.on('click', e => {
+
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height':'calc(100% - 120px)'
+            });
+              
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+
+            this.closeAllMainPanel(true);
+            
+        });
+
+        this.el.btnSendDocument.on('click', e => {
+
+            console.log('Send Document');
+
+        });
+
+        this.el.btnAttachContact.on('click', e => {
+
+            this.el.modalContacts.show();
+ 
+        });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+
+            this.el.modalContacts.hide();
+
+        });
+
+        this.el.btnSendMicrophone.on('click', e => {
+
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+
+        });
+
+        this.el.btnCancelMicrophone.on('click', e => {
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.btnFinishMicrophone.on('click', e => {
+
+            this.closeRecordMicrophone();
+
+        });
+
+        this.el.inputText.on('keypress', e => {
+
+            if (e.key === 'Enter' && !e.crtlKey) {
+
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+
+        });
+
+        this.el.inputText.on('keyup', e => {
+
+            if (this.el.inputText.innerHTML.length) {
+
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+
+            } else {
+
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+
+            };
+
+        });
+
+        this.el.btnSend.on('click', e => {
+
+            console.log(this.el.inputText.innerHTML);
+
+        });
+
+        this.el.btnEmojis.on('click', e => {
+
+            this.el.panelEmojis.toggleClass('open')
+
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+
+            emoji.on('click', e=>{
+
+                let img = this.el.imgEmojiDefault.cloneNode(); // clona o elemento com todas as propriedades
+
+                img.style.cssText = emoji.style.cssText;
+                img.dataset.unicode = emoji.dataset.unicode;
+                img.alt = emoji.dataset.unicode;
+
+                emoji.classList.forEach(name => {
+
+                    img.classList.add(name);
+
+                });
+
+                let cursor = window.getSelection();
+
+                if (!cursor.focusNode || !cursor.focusNode.id == 'input-text') {
+
+                    this.el.inputText.focus();
+                    cursor = window.getSelection();
+
+                };
+
+                let range = document.createRange();
+
+                range = cursor.getRangeAt(0);
+
+                range.deleteContents(); 
+
+                let frag = document.createDocumentFragment();
+
+                frag.appendChild(img);
+                range.insertNode(frag);
+                range.setStartAfter(img);
+
+                this.el.inputText.dispatchEvent(new Event('keyup'));
+                
+            });
+
+        });
+    };
+
+    startRecordMicrophoneTime() {
+
+        let start = Date.now();
+
+        this._recordMicrophoneInterval = setInterval(() => {
+
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+
+        }, 100); // atualiza a cada 100 milisegundos
+
+    };
+
+    closeRecordMicrophone() {
+
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
+
+    };
+
+    closeAllMainPanel(close = false) {
+
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+
+        if (close) this.el.panelMessagesContainer.show();
+
+    };
+
+    closeMenuAttach(e) {
+
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+        console.log('remove menu')
+        
+    };
+
+    closeAllLeftPanel() {
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
+
+    }
+}
